84c747b5c6c006f271f27d629f0c3b636b40f067
Matthew Macartney <matthew.macartney@codecomputerlove.com>
2021-10-19 14:13:39 +0100

    added warnings to output bar a few

--- pa11y.js
+++ pa11y.js
@@ -2,7 +2,16 @@
 const pa11y = require('pa11y');
 
 const url = process.env.URL;
-const options = {}
+const options = {
+    includeWarnings: true,
+    ignore: [
+        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs',
+        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.BgImage',
+        'WCAG2AA.Principle1.Guideline1_4.1_4_10.C32,C31,C33,C38,SCR34,G206',
+        'WCAG2AA.Principle1.Guideline1_4.1_4_3_F24.F24.FGColour'
+    ],
+    hideElements: '[data-tab-content="book-tickets-old"]'
+}
 
 pa11y(url, options).then((results) => {
     console.log(results);
